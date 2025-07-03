0547e2807d66783a20a50ce7597b0dbea6cbec02
dimaz7474 <dimaz.mbs@gmail.com>
2025-07-03 09:42:44 +0700

    tugas javascript
    tugas javascript

---
+++ index.js
@@ -0,0 +1,11 @@
+// Mendeklarasikan variabel dengan var
+var message = "Halo, selamat datang di website kami!";
+
+// Menampilkan pesan di console
+console.log(message);
+
+// Menampilkan pesan di alert box
+alert(message);
+
+// Menampilkan pesan ke dalam elemen HTML (jika ada elemen dengan id 'output')
+document.getElementById("output").innerText = message;
---
+++ tugas1.js
@@ -0,0 +1,17 @@
+
+// script javascript Buatlah 3 buah variabel menggunakan var , let dan const
+// Variabel var berisikan nama depan teman teman , variabel let berisikan nama akhir teman teman , 
+
+// const berisikan umur teman teman saat ini
+// Setelah itu teman teman tampilkan ketiga variabel tersebut dengan console.log()
+
+
+// Deklarasi variabel
+var namaDepan = "Dimas";
+let namaAkhir = "Kurniawan";
+const umur = 37;
+
+// Menampilkan variabel ke console
+console.log("Nama Depan: " + namaDepan);
+console.log("Nama Akhir: " + namaAkhir);
+console.log("Umur: " + umur);
